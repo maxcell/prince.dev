819e5595a2b69f8632d01bd54f6f9374caa5dbaf
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-12-02 22:27:34 -0500

    Adds twitter card metatag

--- src/components/social.js
+++ src/components/social.js
@@ -10,6 +10,7 @@ const Head = (props) => {
   <Helmet defaultTitle={props.title}>
       <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"></link>
       <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700" rel="stylesheet"></link>
+      <meta property="twitter:card" content="summary" />
       <meta property="twitter:description" content={props.description} />
       <meta property="twitter:title" content={props.title} />
   </Helmet>
