85d9a27b2afd87523dada6394be67cf23055ea32
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-12-04 23:09:32 -0500

    Update to include viewport

--- src/components/social.js
+++ src/components/social.js
@@ -8,6 +8,7 @@ const Head = (props) => {
   <Helmet defaultTitle={props.title}>
       <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"></link>
       <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700" rel="stylesheet"></link>
+      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:description" content={props.description} />
       <meta property="twitter:title" content={props.title} />
