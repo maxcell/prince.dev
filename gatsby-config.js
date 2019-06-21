e9d930995d7e32e65703ecb4cc17d29cab5aae4d
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-06-20 20:17:13 -0400

    Adds new blog post

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,11 @@ module.exports = {
     title: 'Prince Wilson - Developer',
     twitter: 'https://twitter.com/maxcell',
     url: 'https://maxcell.me',
-    description: 'An organically growing notebook of thoughts and learnings!'
+    description: 'An organically growing notebook of thoughts and learnings!',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -18,17 +19,25 @@ module.exports = {
               inlineCodeMarker: `>`,
               showLineNumbersGlobal: false,
               noInlineHighlight: false,
-            }
-          }
-        ]
-      }
+            },
+          },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 500,
+              linkImagesToOriginal: false,
+              shopwCaptions: ['title'],
+            },
+          },
+        ],
+      },
     },
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
-        path: `${__dirname}/src`
-      }
+        path: `${__dirname}/src`,
+      },
     },
   ],
 }
