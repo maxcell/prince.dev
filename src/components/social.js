109047e740249e2633c1dd32652af9d9be4f9fbb
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-06-20 23:10:46 -0400

    Updates the blog with new styles and edits post

--- src/components/social.js
+++ src/components/social.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import Helmet from 'react-helmet';
+import React from 'react'
+import Helmet from 'react-helmet'
 import logo from '../assets/maxcell.png'
 
-
-const Head = (props) => {  
- return (
-  <Helmet defaultTitle={props.title}>
-      <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"></link>
-      <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700" rel="stylesheet"></link>
-      <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+const Head = props => {
+  return (
+    <Helmet defaultTitle={props.title}>
+      <link
+        rel="stylesheet"
+        href="//cdn.jsdelivr.net/npm/hack-font@3/build/web/hack.css"
+      />
+      <link
+        href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,700"
+        rel="stylesheet"
+      />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta property="twitter:card" content="summary" />
       <meta property="twitter:description" content={props.description} />
       <meta property="twitter:title" content={props.title} />
-      <meta property="twitter:image" content={"https://maxcell.me" + logo}/>
-      <meta property="og:image" content={logo}/>
-  </Helmet>
-)
+      <meta property="twitter:image" content={'https://maxcell.me' + logo} />
+      <meta property="og:image" content={logo} />
+    </Helmet>
+  )
 }
 
-export default Head;
+export default Head
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,41 +1,42 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import Head from '../components/social'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from '../components/header'
 import '../layouts/index.css'
 
-
 const Layout = ({ children }) => (
-  <StaticQuery 
-  query={graphql`
-    query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title
-        description
-        twitter
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
+            description
+            twitter
+          }
+        }
       }
-    }
-  }`}
-  render={({ site: {siteMetadata}})=> (
-    <Fragment>
-    <Head title={siteMetadata.title} description={siteMetadata.description} />
-    <div className="container" style={{ minHeight: 'inherit', margin: '0 auto', backgroundColor: 'white',width: '70%', maxWidth: '45em',}}>
-    <Header siteTitle={siteMetadata.title} />
-    <hr/>
-    <div
-      style={{
-       
-        height: 'inherit', 
-        padding: '1.45rem 1.0875rem 0',  
-      }}
-    >
-      {children}
-    </div>
-  </div>
-  </Fragment>
-  )}
+    `}
+    render={({ site: { siteMetadata } }) => (
+      <Fragment>
+        <Head
+          title={siteMetadata.title}
+          description={siteMetadata.description}
+        />
+        <div className="container">
+          <Header siteTitle={siteMetadata.title} />
+          <hr />
+          <div
+            style={{
+              padding: '1.45rem 1.0875rem 0',
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </Fragment>
+    )}
   />
 )
 
