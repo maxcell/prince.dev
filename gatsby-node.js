54b22d5692f206c412169ac69a2a3282018371fe
maxcell <maxcell.wilson@gmail.com>
2019-09-16 20:39:57 -0400

    Update files to ensure netlify deploys

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,86 +4,88 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+// You can delete this file if you're not using it
 
- const { createFilePath } = require('gatsby-source-filesystem')
- const path = require('path')
- const _ = require('lodash')
+const { createFilePath } = require('gatsby-source-filesystem')
+const path = require('path')
+const _ = require('lodash')
 
- exports.onCreateNode = ({ node, getNode, actions }) => {
-    const { createNodeField } = actions
-    if(node.internal.type === "MarkdownRemark"){
-        const relativeFilePath = createFilePath({ node, getNode, basePath: 'pages/posts' })
-        let slug = relativeFilePath.split(/([0-9]+\-)/g)
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+  if (node.internal.type === 'MarkdownRemark') {
+    const relativeFilePath = createFilePath({
+      node,
+      getNode,
+      basePath: 'pages/posts',
+    })
+    let slug = relativeFilePath.split(/([0-9]+\-)/g)
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/blog/${slug[slug.length - 1]}`,
+    })
+  }
+}
 
-        createNodeField({
-            node,
-            name: 'slug',
-            value: `/blog/${slug[slug.length - 1]}`
-        })
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
 
+  return graphql(`
+    {
+      allMarkdownRemark(
+        limit: 2000
+        sort: { fields: [frontmatter___date], order: DESC }
+        filter: { frontmatter: { draft: { ne: true } } }
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              tags
+            }
+          }
+        }
+      }
     }
- }
+  `).then(result => {
+    if (result.error) {
+      return Promise.reject(result.errors)
+    }
 
- exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+    const posts = result.data.allMarkdownRemark.edges
 
-    return graphql(`
-        {
-          allMarkdownRemark(
-            limit: 2000
-            sort: { fields: [frontmatter___date], order: DESC}
-            filter: { frontmatter: {draft: { ne: true }}}
-          ) {
-            edges {
-              node {
-                fields {
-                  slug
-                }
-                frontmatter {
-                  tags
-                }
-              }
-            }
-          }
-        }
-      `).then(result => {
-        if (result.error) {
-          return Promise.reject(result.errors)
-        }
-        
-        const posts = result.data.allMarkdownRemark.edges
+    posts.forEach(({ node }) => {
+      post = {
+        path: node.fields.slug,
+        component: path.resolve('./src/templates/blog-post.js'),
+        context: {
+          slug: node.fields.slug,
+        },
+      }
 
-        posts.forEach(({node}) => {
+      createPage(post)
+    })
 
-            post = {
-                path: node.fields.slug,
-                component: path.resolve('./src/templates/blog-post.js'),
-                context: {
-                    slug: node.fields.slug
-                }
-            }
+    let tags = []
 
-            createPage(post)
-        })
+    posts.forEach(edge => {
+      if (edge.node.frontmatter.tags) {
+        tags = [...tags, ...edge.node.frontmatter.tags]
+      }
+    })
 
-        let tags = []
+    tags = [...new Set(tags)]
 
-        posts.forEach(edge => {
-          if(edge.node.frontmatter.tags){
-            tags = [...tags, ...edge.node.frontmatter.tags]
-          }
-        })
-
-        tags = [...new Set(tags)]
-
-        tags.forEach(tag => {
-          createPage({path: `/tags/${_.kebabCase(tag)}`,
-          component: path.resolve('./src/templates/tags.js'),
-          context: {
-            tag
-          }})
-        })
-
+    tags.forEach(tag => {
+      createPage({
+        path: `/tags/${_.kebabCase(tag)}`,
+        component: path.resolve('./src/templates/tags.js'),
+        context: {
+          tag,
+        },
       })
+    })
+  })
 }
---
+++ src/pages/posts/2019-09-10-iterm-scripts.md
@@ -0,0 +1,21 @@
+---
+title: 'Automate the Workflow - iTerm2'
+date: '2019-06-24'
+tags: ['workflow', 'iterm']
+draft: true
+---
+
+Imagine, you're starting your day on a JIRA ticket, you want to understand that weird bug behavior on your local dev environment. However, any time you startup your project on a local environment you need to have at least 3-4 terminal sessions running just for the fullness of your application to work. That becomes tedious to do manually.
+
+In order to be living our best work life, we need to free ourselves of tasks that are frequently by hand. Today, we're gonna learn a bit about building our own custom Python script for [iTerm2](iterm2) to manage this workflow.
+
+#### Prerequisites
+
+- macOSX (v10.12 or above!)
+- Installed [iTerm2](iterm2-downloads) to your system
+- Rudimentary Bash scripting Knowledge (enough to navigate through the terminal)
+
+### Initial Setup
+
+[iterm2]: https://github.com/gnachman/iTerm2
+[iterm2-downloads]: https://www.iterm2.com/downloads.html
