671b908314b8f093e72cf9adc4160dc008a0d4d9
Maxcell Wilson <maxcell.wilson@gmail.com>
2019-11-14 08:28:43 -0500

    Updates bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,14 +11,17 @@ export default () => {
                 or building and securing technology to make people's lives better.
             </p>
             <p>
-                I work as an instructor over at <a href="https://flatironschool.com/">Flatiron School</a>, powered by <a href="https://wework.com/">WeWork</a>. 
-                Some of my previous work includes working at Clarifai and State Farm Insurance. I graduated with a degree in Computer Science 
-                with a focus in security over at the University of Central Florida. Back in school, I founded our tech club with a bunch of my friends,
-                known as Tech Knights. We also created our first intercollegiate hackathon known as Knight Hacks.
+                I previously worked as a software engineer over at <a target="_blank" rel="noopener noreferrer" href="https://flatironschool.com/">Flatiron School</a>, powered by <a target="_blank" rel="noopener noreferrer" href="https://wework.com/">WeWork</a>.
+                I started off as an instructor there and helped over 400 folks transition into a career of technology!
+                Some of my previous work includes working at Clarifai and State Farm Insurance.
             </p>
             <p>
-                Now, I enjoy exploring New York City and really getting to experience a new world! I look for opportunities to give
-                back to communities that helped me become who I am today, through mentorship and open source. But most of all, I really really love corgis.
+                I graduated with a Bachelors degree in Computer Science with a focus in security over at the University of Central Florida. Back in school, I founded our tech club with a bunch of my friends,
+                known as Tech Knights. We also created our first intercollegiate hackathon known as <a target="_blank" rel="noopener noreferrer" href="https://knighthacks.org/">Knight Hacks</a>.
+            </p>
+            <p>
+                Now, I enjoy exploring New York City! I look for opportunities to give back to communities that helped me become who I am today, through mentorship and open source.
+                But most of all, I really really love corgis.
             </p>
             </div>
             </Layout>)
