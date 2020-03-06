433e4d16311058beb9901b0d7224c3ec7689e7a5
Prince Wilson <prince.wilson@newsela.com>
2020-03-05 23:20:37 -0500

    Adds vscode project settings

--- src/components/header.js
+++ src/components/header.js
@@ -3,12 +3,12 @@ import { Link } from 'gatsby'
 
 
 const ListLink = (props) => {
- return (
-  <li style={{ display: 'inline-block', marginRight: '1rem'}}>
-    <Link to={props.to} style={{ color: 'black', textDecoration: 'none', fontWeight: 600, opacity: 0.75 }}>
-      {props.children}
-    </Link>
-  </li>
+  return (
+    <li style={{ display: 'inline-block', marginRight: '1rem' }}>
+      <Link to={props.to} style={{ color: 'black', textDecoration: 'none', fontWeight: 600, opacity: 0.75 }}>
+        {props.children}
+      </Link>
+    </li>
   )
 }
 
@@ -21,10 +21,10 @@ const Header = () => (
         paddingTop: '1.45rem'
       }}
     >
-    <h3 style={{ display: 'inline'}}>
-      <Link to="/" style={{ textDecoration: 'none', color: '#772073', opacity: 0.75 }}>Prince</Link>
-    </h3>
-      <ul style={{ listStyle: 'none', float: 'right'  }} >
+      <h3 style={{ display: 'inline' }}>
+        <Link to="/" style={{ textDecoration: 'none', color: '#772073', opacity: 0.75 }}>Prince</Link>
+      </h3>
+      <ul style={{ listStyle: 'none', float: 'right' }} >
         <ListLink to="/about">About</ListLink>
       </ul>
     </div>
