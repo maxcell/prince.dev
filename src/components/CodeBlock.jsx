0903cd97eaec24bd86ccbbda34392a15615d7a23
Prince Wilson <prince.wilson@newsela.com>
2020-05-02 17:11:03 -0400

    Code Theme Switcher

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+/** @jsx jsx */
+import { useContext } from 'react'
+import { jsx } from '@emotion/core';
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import rangeParser from 'parse-numeric-range';
+
+import { ColorContext } from './ColorSwitcher';
 
 // Create a closure that determines if we have
 // to highlight the given index
@@ -15,6 +19,24 @@ const calculateLinesToHighlight = (meta) => {
   }
 }
 
+/* Fix for my overflowing highlights */
+const wrappingStyles = (codeBlockTheme) => ({
+  backgroundColor: codeBlockTheme ? codeBlockTheme.plain.backgroundColor : '#1d1f21',
+  borderRadius: '0.3em',
+  margin: '2em 0',
+  padding: '1em',
+  overflow: 'auto',
+  boxShadow: 'rgba(0, 0, 0, 0.25) 0px 10px 24px',
+  "& pre[class*='language-']": {
+    backgroundColor: 'transparent',
+    margin: 0,
+    padding: 0,
+    overflow: 'initial',
+    float: 'left',
+    minWidth: '100%'
+  }
+})
+
 
 export default ({ children, className, metastring }) => {
   // Pull the className
@@ -23,29 +45,34 @@ export default ({ children, className, metastring }) => {
   // Determine if we need highlighting
   const shouldHighlightLine = calculateLinesToHighlight(metastring)
 
+  const { codeBlockTheme } = useContext(ColorContext);
+
   return (
-    <Highlight
-      {...defaultProps}
-      code={children}
-      language={language}
-      theme={undefined}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style }}>
-          {tokens.map((line, index) => {
-            const lineProps = getLineProps({ line, key: index })
-            if (shouldHighlightLine(index)) { lineProps.className = `${lineProps.className} highlight-line` }
-            return (
-              <div key={index} {...lineProps}>
-                {line.map((token, key) => (
-                  <span key={key}{...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            )
-          }
-          )}
-        </pre>
-      )}
-    </Highlight>
+    <div css={wrappingStyles(codeBlockTheme)}>
+      <Highlight
+        {...defaultProps}
+        code={children}
+        language={language}
+        theme={codeBlockTheme}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, index) => {
+              const lineProps = getLineProps({ line, key: index })
+              if (shouldHighlightLine(index)) { lineProps.className = `${lineProps.className} highlight-line` }
+              if (line.length === 1 && line[0].empty === true && index === tokens.length - 1) return null;
+              return (
+                <div key={index} {...lineProps}>
+                  {line.map((token, key) => (
+                    <span key={key}{...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              )
+            }
+            )}
+          </pre>
+        )}
+      </Highlight>
+    </div>
   )
 }
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -6,13 +6,14 @@ import { StaticQuery, graphql } from 'gatsby'
 import Header from '../components/header'
 import Link from '../components/Link'
 import CodeBlock from '../components/CodeBlock'
+import { ColorProvider } from '../components/ColorSwitcher'
 import InlineCode from '../components/InlineCode'
 import '../layouts/index.css'
 
 
 const components = {
   a: (props) => <Link {...props} />,
-  pre: (props) => <div className="gatsby-highlight" {...props} />,
+  pre: (props) => <div {...props} />,
   code: CodeBlock,
   inlineCode: InlineCode
 }
@@ -21,8 +22,9 @@ const Layout = ({ children }) => (
   <MDXProvider
     components={components}
   >
-    <StaticQuery
-      query={graphql`
+    <ColorProvider>
+      <StaticQuery
+        query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -33,26 +35,27 @@ const Layout = ({ children }) => (
         }
       }
     `}
-      render={({ site: { siteMetadata } }) => (
-        <Fragment>
-          <Head
-            title={siteMetadata.title}
-            description={siteMetadata.description}
-          />
-          <div className="container">
-            <Header siteTitle={siteMetadata.title} />
-            <main
-              style={{
-                paddingTop: '1.45rem',
-                paddingBottom: '0px'
-              }}
-            >
-              {children}
-            </main>
-          </div>
-        </Fragment>
-      )}
-    />
+        render={({ site: { siteMetadata } }) => (
+          <Fragment>
+            <Head
+              title={siteMetadata.title}
+              description={siteMetadata.description}
+            />
+            <div className="container">
+              <Header siteTitle={siteMetadata.title} />
+              <main
+                style={{
+                  paddingTop: '1.45rem',
+                  paddingBottom: '0px'
+                }}
+              >
+                {children}
+              </main>
+            </div>
+          </Fragment>
+        )}
+      />
+    </ColorProvider>
   </MDXProvider>
 )
 
