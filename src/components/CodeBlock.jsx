f7af7bb0cf44d7af67a28bde8b67e304b1abe555
Prince Wilson <prince.wilson@newsela.com>
2020-07-23 18:59:06 -0400

    PoC for rust playground embedded

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import rangeParser from 'parse-numeric-range';
 
+import { getParameters } from 'codesandbox/lib/api/define';
+
 // Create a closure that determines if we have
 // to highlight the given index
 const calculateLinesToHighlight = (meta) => {
@@ -16,36 +18,55 @@ const calculateLinesToHighlight = (meta) => {
 }
 
 
-export default ({ children, className, metastring }) => {
+export default ({ children, className, metastring, playground }) => {
   // Pull the className
   const language = className?.replace(/language-/, '') || ""
 
   // Determine if we need highlighting
   const shouldHighlightLine = calculateLinesToHighlight(metastring)
+  let url;
+  if (playground && language === 'rust') {
+    const playgroundBaseURL = "https://play.rust-lang.org/?version=stable&mode=debug&edition=2018&code="
+    const encodedCode = encodeURI(children)
+    url = playgroundBaseURL + encodedCode
+
+
+
+    return (
+      <iframe
+        src={url}
+        loading='lazy'>
+
+      </iframe>
+    )
+  }
 
   return (
-    <Highlight
-      {...defaultProps}
-      code={children}
-      language={language}
-      theme={undefined}
-    >
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style }}>
-          {tokens.map((line, index) => {
-            const lineProps = getLineProps({ line, key: index })
-            if (shouldHighlightLine(index)) { lineProps.className = `${lineProps.className} highlight-line` }
-            return (
-              <div key={index} {...lineProps}>
-                {line.map((token, key) => (
-                  <span key={key}{...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            )
-          }
-          )}
-        </pre>
-      )}
-    </Highlight>
+    <React.Fragment>
+      {playground && <a style={{ color: 'white', marginBottom: '30px' }} href={url}>Go to the playground</a>}
+      <Highlight
+        {...defaultProps}
+        code={children}
+        language={language}
+        theme={undefined}
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={{ ...style }}>
+            {tokens.map((line, index) => {
+              const lineProps = getLineProps({ line, key: index })
+              if (shouldHighlightLine(index)) { lineProps.className = `${lineProps.className} highlight-line` }
+              return (
+                <div key={index} {...lineProps}>
+                  {line.map((token, key) => (
+                    <span key={key}{...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              )
+            }
+            )}
+          </pre>
+        )}
+      </Highlight>
+    </React.Fragment>
   )
 }
